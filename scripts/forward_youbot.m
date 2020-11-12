51e87eee54c6a73ee1d5bce0730c913a340db1c2
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2020-11-12 13:55:38 +0100

    added function interface

--- scripts/forward_youbot.m
+++ scripts/forward_youbot.m
@@ -1,62 +1,60 @@
 % RoboterSysteme WS2020/21
 % ForwardKinematics KUKA youbot
-% 11.11.2020 Gerald Hebinck
+% 12.11.2020 Gerald Hebinck
+% takes vector of angles, returns transformation matrix
 
-clc;
-clear all;
-
-% theta_ := rotation angles
-% theta_offset := angle zero points
-% DH_array := Denavit-Hartenberg-values
-% eecp = endEffectorCenterPoint
-theta_ = [ 0 0 0 0 0];
-theta_offset = [ 0 pi/2 0 pi/2 0];
-DH_array = [  33, pi/2,   147, theta_(1)+theta_offset(1);
-             155,    0,     0, theta_(2)+theta_offset(2);
-             135,    0,     0, theta_(3)+theta_offset(3);
-               0, pi/2,     0, theta_(4)+theta_offset(4);
-               0,    0, 217.5, theta_(5)+theta_offset(5)    
-           ];
-eecp = 1;
+function eecp = forward_youbot(theta_)
 
 
-% calculation
-for c = 1:5
-    % get params from DH_array
-    a = DH_array(c,1);
-    alpha = DH_array(c,2);
-    l = DH_array(c,3);
-    theta = DH_array(c,4);
-    
-    % build translation and rotation matrices
-    rot_axis = [
-                         1,          0,          0,          0;
-                         0, cos(alpha),-sin(alpha),          0;
-                         0, sin(alpha), cos(alpha),          0;
-                         0,          0,          0,          1
+    % theta_ := rotation angles
+    % theta_offset := angle zero points
+    % DH_array := Denavit-Hartenberg-values
+    % eecp = endEffectorCenterPoint
+    % theta_ = input param;
+    theta_offset = [ 0 pi/2 0 pi/2 0];
+    DH_array = [  33, pi/2,   147, theta_(1)+theta_offset(1);
+                 155,    0,     0, theta_(2)+theta_offset(2);
+                 135,    0,     0, theta_(3)+theta_offset(3);
+                   0, pi/2,     0, theta_(4)+theta_offset(4);
+                   0,    0, 217.5, theta_(5)+theta_offset(5)    
                ];
-    rot_angle = [
-                cos(theta),-sin(theta),          0,          0;
-                sin(theta), cos(theta),          0,          0;
-                         0,          0,          1,          0;
-                         0,          0,          0,          1
-                ];
-    trans_length = [
-                         1,          0,          0,          0;
-                         0,          1,          0,          0;
-                         0,          0,          1,          l;
-                         0,          0,          0,          1
-                   ];
-    trans_offset = [
-                         1,          0,          0,          a;
-                         0,          1,          0,          0;
-                         0,          0,          1,          0;
-                         0,          0,          0,          1
-                   ];    
-    
-    % apply matrices to previous value
-    eecp = eecp * rot_angle * trans_length * trans_offset * rot_axis;
-end
+    eecp = 1;
 
-% output
-eecp(1:3,4)
+    % calculation
+    for c = 1:5
+        % get params from DH_array
+        a = DH_array(c,1);
+        alpha = DH_array(c,2);
+        l = DH_array(c,3);
+        theta = DH_array(c,4);
+
+        % build translation and rotation matrices
+        rot_axis = [
+                             1,          0,          0,          0;
+                             0, cos(alpha),-sin(alpha),          0;
+                             0, sin(alpha), cos(alpha),          0;
+                             0,          0,          0,          1
+                   ];
+        rot_angle = [
+                    cos(theta),-sin(theta),          0,          0;
+                    sin(theta), cos(theta),          0,          0;
+                             0,          0,          1,          0;
+                             0,          0,          0,          1
+                    ];
+        trans_length = [
+                             1,          0,          0,          0;
+                             0,          1,          0,          0;
+                             0,          0,          1,          l;
+                             0,          0,          0,          1
+                       ];
+        trans_offset = [
+                             1,          0,          0,          a;
+                             0,          1,          0,          0;
+                             0,          0,          1,          0;
+                             0,          0,          0,          1
+                       ];    
+
+        % apply matrices to previous value
+        eecp = eecp * rot_angle * trans_length * trans_offset * rot_axis;
+    end
+end
